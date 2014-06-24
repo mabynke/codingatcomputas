107a1e6acde9b40fa3af5ee79447901ff6e761f6
Mathias Bynke <mabynke@hotmail.com>
2014-06-24 19:05:19 +0200

    Update server.js
    Minne

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    // 200 betyr ok på internett (eller, http, da)
+    // 200 betyr ok pï¿½ internett (eller, http, da)
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hei \n');
 }).listen(process.env.port);
+
+//Mathias var her!
